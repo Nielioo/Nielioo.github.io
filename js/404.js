c3759948987fd646cb3dece49153b14d78119ca4
Daniel Aprillio <70996943+Nielioo@users.noreply.github.com>
2023-01-18 07:32:49 +0700

    change 404 page

---
+++ js/404.js
@@ -0,0 +1,20 @@
+//Inspired from https://dribbble.com/shots/3913847-404-page
+
+var pageX = $(document).width();
+var pageY = $(document).height();
+var mouseY = 0;
+var mouseX = 0;
+
+$(document).mousemove(function (event) {
+    //verticalAxis
+    mouseY = event.pageY;
+    yAxis = (pageY - mouseY) / pageY * 300;
+    //horizontalAxis
+    mouseX = event.pageX / -pageX;
+    xAxis = -mouseX * 100 - 100;
+
+    $('.box__ghost-eyes').css({ 'transform': 'translate(' + xAxis + '%,-' + yAxis + '%)' });
+
+    // console.log('X: ' + xAxis);
+
+});
