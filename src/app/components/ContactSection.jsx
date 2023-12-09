7014d512c379eefd00c25aa0e1cd8212313f4101
Daniel Aprillio <70996943+Nielioo@users.noreply.github.com>
2023-12-09 10:48:13 +0700

    feat: fix socials icon

--- src/app/components/ContactSection.jsx
+++ src/app/components/ContactSection.jsx
@@ -2,14 +2,12 @@
 import React from "react";
 import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub, faLinkedin, faInstagram, faSquareFacebook, faLine, faTelegram, faDiscord } from "@fortawesome/free-brands-svg-icons";
+import { faLine } from "@fortawesome/free-brands-svg-icons";
+import { SocialIcon } from "react-social-icons";
 
 const AboutSection = () => {
    return (
-      <section
-         id="contact"
-         className="my-12 md:my-12 py-24 gap-4 relative"
-      >
+      <section id="contact" className="my-12 md:my-12 py-24 gap-4 relative">
          <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-10 blur-lg absolute top-3/4 -left-0 transform -translate-x-1/2 -translate-1/2 overflow-hidden"></div>
          <div className="z-20">
             <h5 className="text-2xl font-bold text-white my-2">
@@ -19,32 +17,59 @@ const AboutSection = () => {
                {" "}
                I&apos;m currently looking for new opportunities, my inbox is
                always open.
-               <br/>
-               Whether you have a question or just want to say hi,
-               I&apos;ll try my best to get back to you!
+               <br />
+               Whether you have a question or just want to say hi, I&apos;ll try
+               my best to get back to you!
             </p>
-            <div className="socials flex flex-row text-white gap-3">
-               <Link href="https://github.com/Nielioo">
-                  <FontAwesomeIcon icon={faGithub} alt="Github Icon" size="lg" />
-               </Link>
-               <Link href="https://www.linkedin.com/in/daniel-aprillio/">
-                  <FontAwesomeIcon icon={faLinkedin} alt="LinkedIn Icon" size="lg" />
-               </Link>
-               <Link href="https://www.instagram.com/daniel_aprillio/">
-                  <FontAwesomeIcon icon={faInstagram} alt="Instagram Icon" size="lg" />
-               </Link>
-               <Link href="https://www.facebook.com/nielioo">
-                  <FontAwesomeIcon icon={faSquareFacebook} alt="Facebook Icon" size="lg" />
-               </Link>
+            <div className="socials flex flex-row text-white gap-2">
+               <SocialIcon
+                  url="https://github.com/Nielioo"
+                  network="github"
+                  bgColor="#FFFFFF"
+                  fgColor="#000000"
+                  style={{ height: 32, width: 32 }}
+               />
+               <SocialIcon
+                  url="https://www.linkedin.com/in/daniel-aprillio/"
+                  network="linkedin"
+                  bgColor="#FFFFFF"
+                  fgColor="#000000"
+                  style={{ height: 32, width: 32 }}
+               />
+               <SocialIcon
+                  url="https://www.instagram.com/daniel_aprillio/"
+                  network="instagram"
+                  bgColor="#FFFFFF"
+                  fgColor="#000000"
+                  style={{ height: 32, width: 32 }}
+               />
+               <SocialIcon
+                  url="https://www.facebook.com/nielioo"
+                  network="facebook"
+                  bgColor="#FFFFFF"
+                  fgColor="#000000"
+                  style={{ height: 32, width: 32 }}
+               />
                <Link href="https://line.me/ti/p/~daniel_aprillio">
-                  <FontAwesomeIcon icon={faLine} alt="LINE Icon" size="lg" />
+                  <FontAwesomeIcon
+                     icon={faLine}
+                     style={{ height: 32, width: 32 }}
+                  />
                </Link>
-               <Link href="https://t.me/Nielioo">
-                  <FontAwesomeIcon icon={faTelegram} alt="Telegram Icon" size="lg" />
-               </Link>
-               <Link href="https://discord.com/users/551736880014819329">
-                  <FontAwesomeIcon icon={faDiscord} alt="Discord Icon" size="lg" />
-               </Link>
+               <SocialIcon
+                  url="https://t.me/Nielioo"
+                  network="telegram"
+                  bgColor="#FFFFFF"
+                  fgColor="#000000"
+                  style={{ height: 32, width: 32 }}
+               />
+               <SocialIcon
+                  url="https://discord.com/users/551736880014819329"
+                  network="discord"
+                  bgColor="#FFFFFF"
+                  fgColor="#000000"
+                  style={{ height: 32, width: 32 }}
+               />
             </div>
          </div>
       </section>
