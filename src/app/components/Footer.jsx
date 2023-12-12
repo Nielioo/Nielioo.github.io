c591f2a19a260ca033ea336d2a5aa98952fb2db0
Daniel Aprillio <70996943+Nielioo@users.noreply.github.com>
2023-12-12 13:19:57 +0700

    feat: fix ui on mobile

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#575a6b] border-l-transparent border-r-transparent border-b-transparent text-white">
       <div className="container p-20 flex justify-center">
-        <p className="text-slate-600">© 2023 Daniel Aprillio a.k.a Nielio</p>
+        <p className="text-slate-600 whitespace-nowrap">© 2023 Daniel Aprillio a.k.a Nielio</p>
       </div>
     </footer>
   );
